70a32120163b8ce4780c41cdc17f6ecedb2697ca
Kevin Kelley <kkelley622@gmail.com>
2022-01-05 12:18:37 -0600

    commit tree 44d5cfe24f6cbdb66933e07acda57282bb45b404

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution here
+
+const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+function findMatching(drivers, string) {
+    const result = drivers.filter(element => element.toLowerCase() === string.toLowerCase())
+    return result
+}
+
+function fuzzyMatch(drivers, string) {
+    const driverLetters = drivers.filter(element => element[0] === string[0])
+    return driverLetters
+}
+
+
+function matchName(drivers, string) {
+    const nameHometown = drivers.filter(element => element.name === string)
+    return nameHometown
+}
